1fae7bda5f3abb9d1d547f573708a5b93a67706b
Leyla <157950987+leylamaria@users.noreply.github.com>
2024-02-19 10:38:54 +0100

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateDestination(event) {
+  event.preventDefault();
 
+  new Typewriter("#destination", {
+    strings: "Brazil, sunny, warm waters and steady winds",
+    autoStart: true,
+    cursur: null,
+    delay: 10,
+  });
+}
+
+let destinationFormElement = document.querySelector(
+  "#destination-generator-form"
+);
+destinationFormElement.addEventListener("submit", generateDestination);
